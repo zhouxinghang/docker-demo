ed7866bdafece4ffd11e1e44fc22d866946b2c83
zhouxinghang <zhouxinghang@kuaishou.com>
2021-12-02 10:58:25 +0800

    host info

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,5 @@ FROM openjdk:8-jdk-alpine
 RUN echo "Asia/Shanghai" > /etc/timezone
 COPY ./target/*.jar docker-demo.jar
 ENTRYPOINT ["java","-jar","docker-demo.jar"]
+
+## https://www.baeldung.com/dockerizing-spring-boot-application
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
     @Value("${app.env}")
     private String env;
 
     @GetMapping("/")
     public String hello() {
-        return "hello k8s, env:" + env;
+        return "hello k8s, env:" + env + ", ip:" +  localhost();
+    }
+
+    private String localhost() {
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            return address.getHostAddress();
+        } catch (UnknownHostException e) {
+            log.error("getLocalHost error", e);
+            return "unknown";
+        }
     }
 
 }
